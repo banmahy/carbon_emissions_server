a757549b1f86302b9d4a321fbf1540443fc96e67
yhy <953613468@qq.com>
2023-04-03 00:18:46 +0800

    V1

---
+++ src/main/java/com/example/ceserver/controller/WeChatLoginController.java
@@ -0,0 +1,26 @@
+package com.example.ceserver.controller;
+
+import com.example.ceserver.service.WeChatLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName WeChatLoginController
+ * @Author
+ * @Date
+ **/
+@RestController
+@RequestMapping("api")
+public class WeChatLoginController {
+
+    @Autowired
+    private WeChatLoginService weChatLoginService;
+
+    @RequestMapping("/login")
+    public ResponseEntity<String> weChatLogin(String code) {
+        return ResponseEntity.ok(weChatLoginService.login(code));
+    }
+
+}
